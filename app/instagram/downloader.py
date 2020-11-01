f7953f355f180f07d271eb407fe30ec43a9783f3
normaloide <edoardoalejandroferrari@gmail.com>
2020-11-01 03:56:05 +0100

    Update README.md + fix

--- app/instagram/downloader.py
+++ app/instagram/downloader.py
@@ -14,7 +14,8 @@ def get_user_data(ig_user):
 		download_videos=False,
 		download_video_thumbnails=False,
 		download_comments=False,
-		compress_json=False
+		compress_json=False,
+    		request_timeout=2
 	)
 
 	loader.download_profile(profile_name=username)
