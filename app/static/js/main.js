f1c07cbd62d54f919716b69eafc5554dde9dba7d
Nespoli <nespoli.bt@gmail.com>
2020-11-01 16:42:16 +0100

    :lipstick: New Login page and map popup

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,31 +2,37 @@ const L = require("leaflet");
 const axios = require("axios");
 
 document.addEventListener("DOMContentLoaded", function () {
-  // initialize Leaflet
-  var map = L.map("mapid").setView({ lon: 0, lat: 0 }, 2);
-
-  // add the OpenStreetMap tiles
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-  }).addTo(map);
-
-  // show the scale bar on the lower left corner
-  L.control.scale().addTo(map);
-
   const sendButton = document.getElementById("send");
   const username = document.getElementById("username");
+  const loading = document.getElementById("loading");
+  const app = document.getElementById("app");
 
   sendButton.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log(username.value);
+    loading.classList.add("loading");
+
     axios
-      .get("/run:5000", {
-        iguser: username.value,
+      .get("/run", {
+        params: {
+          iguser: username.value,
+        },
       })
-      .then(function (response) {
-        console.log(response);
+      .then(function ({ data }) {
+        loading.classList.remove("loading");
+        console.log(data);
+        app.classList.add("app");
+        // initialize Leaflet
+        var map = L.map("map").setView({ lon: 0, lat: 0 }, 2);
+
+        // add the OpenStreetMap tiles
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+        }).addTo(map);
+
+        // show the scale bar on the lower left corner
+        L.control.scale().addTo(map);
       });
   });
 });
